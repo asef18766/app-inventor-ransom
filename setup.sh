eba1d3426aece012d13ec9f5866dc192fd63b725
unknown <asef18766@gmail.com>
2022-04-10 18:48:39 +0800

    add general shell support & fix newgrp

--- setup.sh
+++ setup.sh
@@ -6,13 +6,13 @@ sudo usermod -aG docker $USER
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install vagrant
-newgrp docker
+newgrp docker << END
 
 # cloning app inventor source & patch
 git clone https://github.com/mit-cml/appinventor-sources.git
 mv Vagrantfile appinventor-sources/Vagrantfile
-pushd appinventor-sources
+cd appinventor-sources
 echo "apt install -y git" >> bootstrap.sh
 vagrant up --provider=docker
 git submodule update --init
-popd
+cd ..
